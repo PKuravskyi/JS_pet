0e32cc979738eaae38f8d9a8535165608403dfc1
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-04 18:59:34 +0200

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'src', 'projectPlanner', 'App.js'),
+	output: {
+		path: path.resolve(__dirname, 'projectPlanner', 'assets', 'scripts'),
+		filename: '[name].[contenthash].js',
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'projectPlanner', 'index.html'),
+		}),
+	],
+	devServer: {
+		static: path.resolve(__dirname, 'projectPlanner'),
+		open: true,
+	},
+};
