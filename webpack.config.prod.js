61f32fed4c2f19c5b5a38efe76b201fbebc75651
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-06 14:20:46 +0200

    add dependencies and babel

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -9,6 +9,25 @@ module.exports = {
 		filename: '[contenthash].js',
 		clean: true,
 	},
+	module: {
+		rules: [
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							[
+								'@babel/preset-env',
+								{ useBuiltIns: 'usage', corejs: { version: 3 } },
+							],
+						],
+					},
+				},
+			},
+		],
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'projectPlanner', 'index.html'),
