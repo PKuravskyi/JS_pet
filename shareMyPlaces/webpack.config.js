6d12917aec32f38bfed59c964be2eb20e7719e1a
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-22 00:23:10 +0200

    [shareMyPlaces] modify webpack configs

--- shareMyPlaces/webpack.config.js
+++ shareMyPlaces/webpack.config.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
+	// USE THIS ENTRY STRUCTURE WHEN USING MULTIPLE ENTRIES!!!
 	entry: {
-		MyPlace: path.resolve(__dirname, 'src', 'MyPlace.js'),
-		SharePlace: path.resolve(__dirname, 'src', 'SharePlace.js'),
+		MyPlace: [
+			path.resolve(__dirname, 'src', 'MyPlace.js'),
+			path.resolve(__dirname, 'src', 'SharePlace.js'),
+		],
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist', 'assets', 'scripts'),
