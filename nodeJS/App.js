96142d6888d09b4ab3c2630a9a6368cbd9161381
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-22 00:24:41 +0200

    add nodeJS project

---
+++ nodeJS/App.js
@@ -0,0 +1,93 @@
+// ************************ FILES ************************
+const fs = require('fs');
+
+const userDataPath = 'user-data.txt';
+
+fs.writeFile(userDataPath, 'username=Pavlo', err => {
+	err ? console.log(err) : console.log('Wrote to file!');
+});
+
+fs.readFile(userDataPath, (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+	console.log(`${userDataPath} data =>`, data.toString());
+});
+
+// ************************ HTTP in old-school way ************************
+const http = require('http');
+
+const httpServer = http.createServer((request, response) => {
+	let body = [];
+	request.on('data', chunk => {
+		body.push(chunk);
+	});
+	request.on('end', () => {
+		body = Buffer.concat(body).toString();
+		let userName = 'Unknown User';
+		if (body) {
+			userName = body.split('=')[1];
+		}
+		response.setHeader('Content-Type', 'text/html');
+		response.write(
+			`<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"/><button type="submit">Send</button></form>`
+		);
+		response.end();
+	});
+});
+
+httpServer.listen(3000, () => {
+	console.log('Server for HTTP testing is running on http://localhost:3000/');
+});
+
+// ************************ HTTP with Express.js ************************
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// each use() is a middleware
+// use() order matters! executes one by one in given order
+app.use((request, response, next) => {
+	response.setHeader('Contenty-Type', 'text/html');
+	// execute next use() middleware
+	next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((request, response) => {
+	const userName = request.body.username || 'Unknown User';
+	response.render('index', {
+		user: userName,
+	});
+});
+
+app.listen(3001, () => {
+	console.log(
+		'Server for Express.js testing is running on http://localhost:3001/'
+	);
+});
+
+// ************************ REST
+const locationRoutes = require('./routes/Location');
+
+const appRest = express();
+appRest.use(bodyParser.json());
+
+appRest.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+appRest.use(locationRoutes);
+
+appRest.listen(3002, () => {
+	console.log('Server for REST testing is running on http://localhost:3002/');
+});
---
+++ nodeJS/routes/Location.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const mongoUri =
+	'mongodb+srv://test_user:test_user@testcluster.ybs6i9t.mongodb.net/locations?retryWrites=true&w=majority';
+
+const client = new MongoClient(mongoUri);
+
+const router = express.Router();
+
+router.post('/add-location', async (req, res) => {
+	try {
+		await client.connect();
+		const database = client.db('locations');
+		const collection = database.collection('user-locations');
+
+		const result = await collection.insertOne({
+			address: req.body.address,
+			coords: { lat: req.body.lat, lng: req.body.lng },
+		});
+		res.json({ message: 'Stored location!', locationId: result.insertedId });
+		console.log(result.insertedId);
+	} catch (err) {
+		console.dir(err);
+	} finally {
+		//client.close();
+	}
+});
+
+router.get('/location/:lid', async (req, res) => {
+	const locationId = req.params.lid;
+
+	try {
+		await client.connect();
+		const database = client.db('locations');
+		const collection = database.collection('user-locations');
+
+		const location = await collection.findOne({
+			_id: new ObjectId(locationId),
+		});
+
+		if (!location) {
+			return res.status(404).json({ message: 'Location not found!' });
+		}
+		res.json({ address: location.address, coordinates: location.coords });
+	} catch (err) {
+		console.dir(err);
+	} finally {
+		//client.close();
+	}
+});
+
+module.exports = router;
