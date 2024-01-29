c81af92cda730494c80e593dc20a81e79f3153e2
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-29 20:37:35 +0200

    add cypress Commands

--- shareMyPlaces/cypress/support/commands.js
+++ shareMyPlaces/cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('verifyAddressTitleNotEmpty', () => {
+	cy.get('#address-title').should('not.be.empty');
+});
+
+Cypress.Commands.addQuery('getByDataCyId', id => {
+	const getByDataCyId = cy.now('get', `[data-cy="${id}"]`);
+	return () => getByDataCyId();
+});
