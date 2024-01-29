fcb8a17fa88c3399623d8fdafc1b26f51d547c4c
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-29 20:20:30 +0200

    add cypress browser permission dependency change base url

--- shareMyPlaces/cypress.config.js
+++ shareMyPlaces/cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require('cypress');
+const {
+	cypressBrowserPermissionsPlugin,
+} = require('cypress-browser-permissions');
 
 module.exports = defineConfig({
+	env: {
+		// doesn't work on default Electron browser,
+		// run with "npx cypress run --browser chrome"
+		browserPermissions: {
+			geolocation: 'allow',
+		},
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			config = cypressBrowserPermissionsPlugin(on, config);
+			return config;
 		},
+		baseUrl: 'https://petjs-sharemyplaces-61e5d.web.app',
 	},
 });
