28481cf2855376beac104effc3f1fa3454b05bb1
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-30 17:33:17 +0200

    update tests to use stubs, clocks

--- shareMyPlaces/cypress/e2e/sharePlaces.cy.js
+++ shareMyPlaces/cypress/e2e/sharePlaces.cy.js
@@ -2,17 +2,35 @@
 
 describe('Share a Places', () => {
 	beforeEach(() => {
-		cy.visit('/');
+		cy.clock();
+		cy.fixture('coordinates.json').as('coords');
+		cy.visit('/').then(win => {
+			cy.get('@coords').then(coords => {
+				cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+					.as('getUserLocation')
+					.callsFake(
+						successCallback => {
+							successCallback(coords);
+						},
+						errorCallback => {
+							errorCallback(new Error('Geolocation error'));
+						}
+					);
+			});
+		});
 	});
 
 	it('should be possible to find place by address', () => {
 		cy.getByDataCyId('address-input').type('Ivano-Frankivsk');
 		cy.get('button').contains('Find Place').click();
+		cy.tick(3000);
 		cy.verifyAddressTitleNotEmpty();
 	});
 
 	it('should be possible to find user location', () => {
 		cy.getByDataCyId('locate-btn').click();
+		cy.tick(3000);
+		cy.get('@getUserLocation').should('have.been.called');
 		cy.verifyAddressTitleNotEmpty();
 	});
 });
