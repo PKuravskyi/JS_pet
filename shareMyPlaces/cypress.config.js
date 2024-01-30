ec11d4c397a6d37a45eee749056b693dec23ad29
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-30 17:32:39 +0200

    revert cypress configs

--- shareMyPlaces/cypress.config.js
+++ shareMyPlaces/cypress.config.js
@@ -1,21 +1,8 @@
 const { defineConfig } = require('cypress');
-const {
-	cypressBrowserPermissionsPlugin,
-} = require('cypress-browser-permissions');
 
 module.exports = defineConfig({
-	env: {
-		// doesn't work on default Electron browser,
-		// run with "npx cypress run --browser chrome"
-		browserPermissions: {
-			geolocation: 'allow',
-		},
-	},
 	e2e: {
-		setupNodeEvents(on, config) {
-			config = cypressBrowserPermissionsPlugin(on, config);
-			return config;
-		},
+		setupNodeEvents(on, config) {},
 		baseUrl: 'https://petjs-sharemyplaces-61e5d.web.app',
 	},
 });
