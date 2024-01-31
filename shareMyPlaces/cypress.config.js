4b92b33ba719b3d190f0d011e3d8438d89779a19
Pavlo Kuravskyi <pkuravsky@gmail.com>
2024-01-31 18:36:27 +0200

    update cy configs

--- shareMyPlaces/cypress.config.js
+++ shareMyPlaces/cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+	viewportWidth: 1920,
+	viewportHeight: 1080,
 	e2e: {
-		setupNodeEvents(on, config) {},
 		baseUrl: 'https://petjs-sharemyplaces-61e5d.web.app',
+		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+		setupNodeEvents(on, config) {},
 	},
 });
